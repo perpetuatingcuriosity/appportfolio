794e90181c8dda873fa535ec6cd6a1c31d2a6020
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2015-06-14 22:09:33 -0400

    add styles + button functionality

--- app/src/main/java/com/example/pcurio/appportfolio/MainActivity.java
+++ app/src/main/java/com/example/pcurio/appportfolio/MainActivity.java
@@ -1,19 +1,69 @@
 package com.example.pcurio.appportfolio;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private Context mContext;
+
+    //UI Elements
+    private Button mSpotify;
+    private Button mScores;
+    private Button mLibrary;
+    private Button mBuildBigger;
+    private Button mXyzReader;
+    private Button mCapstone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        mContext = this;
+
+        //UI Elements
+        mSpotify = (Button) findViewById(R.id.main_button_spotify);
+        mScores = (Button) findViewById(R.id.main_button_scores);
+        mLibrary = (Button) findViewById(R.id.main_button_library);
+        mBuildBigger = (Button) findViewById(R.id.main_button_build_bigger);
+        mXyzReader = (Button) findViewById(R.id.main_button_xyz);
+        mCapstone = (Button) findViewById(R.id.main_button_capstone);
+
+        ArrayList<Button> buttonList = new ArrayList<>();
+        buttonList.add(mSpotify);
+        buttonList.add(mScores);
+        buttonList.add(mLibrary);
+        buttonList.add(mBuildBigger);
+        buttonList.add(mXyzReader);
+        buttonList.add(mCapstone);
+
+        for(Button button : buttonList){
+            setButtonClickListener(button, button.getText().toString());
+        }
+
+    } //onCreate
+
+    public void setButtonClickListener(Button button, final String title){
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "This button will launch " + title + "!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    } //setButtonClickListener
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
